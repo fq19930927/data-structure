2e1cdd4833a3cc40d071800796616fee2cadd050
fuqiang <18251359382@163.com>
2018-11-09 21:23:06 +0800

    数组

--- src/main/java/com/example/datastructure/array/Array.java
+++ src/main/java/com/example/datastructure/array/Array.java
@@ -4,13 +4,13 @@ package com.example.datastructure.array;
  * @author fuqiang
  * @version Array, v0.1 2018/11/8 21:13
  */
-public class Array {
+public class Array<E> {
 
-    private int[] data;
+    private E[] data;
     private int size;
 
     public Array(int capacity) {
-        data = new int[capacity];
+        data = (E[]) new Object[capacity];
         size = 0;
     }
 
@@ -35,17 +35,17 @@ public class Array {
     }
 
     // 向所有元素后添加一个新元素
-    public void addLast(int e){
+    public void addLast(E e){
         add(size, e);
     }
 
     // 在所有元素前添加一个新元素
-    public void addFirst(int e){
+    public void addFirst(E e){
         add(0, e);
     }
 
     // 在index索引的位置插入一个新元素e
-    public void add(int index, int e) {
+    public void add(int index, E e) {
         if(size == data.length)
             throw new IllegalArgumentException("Add failed. Array is full.");
 
@@ -61,21 +61,21 @@ public class Array {
     }
 
     // 获取index索引位置的元素
-    public int get(int index){
+    public E get(int index){
         if(index < 0 || index >= size)
             throw new IllegalArgumentException("Get failed. Index is illegal.");
         return data[index];
     }
 
     // 修改index索引位置的元素为e
-    public void set(int index, int e){
+    public void set(int index, E e){
         if(index < 0 || index >= size)
             throw new IllegalArgumentException("Set failed. Index is illegal.");
         data[index] = e;
     }
 
     // 查找数组中是否有元素e
-    public boolean contains(int e){
+    public boolean contains(E e){
         for(int i = 0 ; i < size ; i ++){
             if(data[i] == e)
                 return true;
@@ -84,7 +84,7 @@ public class Array {
     }
 
     // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
-    public int find(int e){
+    public int find(E e){
         for(int i = 0 ; i < size ; i ++){
             if(data[i] == e)
                 return i;
@@ -93,11 +93,11 @@ public class Array {
     }
 
     // 从数组中删除index位置的元素, 返回删除的元素
-    public int remove(int index){
+    public E remove(int index){
         if(index < 0 || index >= size)
             throw new IllegalArgumentException("Remove failed. Index is illegal.");
 
-        int ret = data[index];
+        E ret = data[index];
         for(int i = index + 1 ; i < size ; i ++)
             data[i - 1] = data[i];
         size --;
@@ -105,17 +105,17 @@ public class Array {
     }
 
     // 从数组中删除第一个元素, 返回删除的元素
-    public int removeFirst(){
+    public E removeFirst(){
         return remove(0);
     }
 
     // 从数组中删除最后一个元素, 返回删除的元素
-    public int removeLast(){
+    public E removeLast(){
         return remove(size - 1);
     }
 
     // 从数组中删除元素e
-    public void removeElement(int e){
+    public void removeElement(E e){
         int index = find(e);
         if(index != -1)
             remove(index);
---
+++ src/main/java/com/example/datastructure/array/Student.java
@@ -0,0 +1,30 @@
+package com.example.datastructure.array;
+
+/**
+ * @author fuqiang
+ * @version Student, v0.1 2018/11/9 21:21
+ */
+public class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Student(name: %s, score: %d)", name, score);
+    }
+
+    public static void main(String[] args) {
+
+        Array<Student> arr = new Array<>();
+        arr.addLast(new Student("Alice", 100));
+        arr.addLast(new Student("Bob", 66));
+        arr.addLast(new Student("Charlie", 88));
+        System.out.println(arr);
+    }
+
+}
